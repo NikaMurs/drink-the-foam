4bbb5c86dbe0641f8296a9692e695ed551c223ad
Akim Vakhrenev <akeem3159119@gmail.com>
2024-12-29 18:48:20 +0300

    Updates

---
+++ precache-manifest.8ed0dc5c5e01ddbdf36eeb8e30ba2fd7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51dead56409f0c23d7ed2e5f06cfa797",
+    "url": "/drink-the-foam/index.html"
+  },
+  {
+    "revision": "570d45d9d45a9fad26a6",
+    "url": "/drink-the-foam/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "ff578994475da1cd1538",
+    "url": "/drink-the-foam/static/js/2.0d3afb6d.chunk.js"
+  },
+  {
+    "revision": "570d45d9d45a9fad26a6",
+    "url": "/drink-the-foam/static/js/main.aa73fb9e.chunk.js"
+  },
+  {
+    "revision": "7df550e786095f22520e",
+    "url": "/drink-the-foam/static/js/runtime~main.6cdd2ba0.js"
+  }
+]);
