56d9e67ea6848d7cd74d26667fef831872156b2f
Akim Vakhrenev <akeem3159119@gmail.com>
2024-12-30 02:04:16 +0300

    update penalties

--- src/constants/penalties.js
+++ src/constants/penalties.js
@@ -6,8 +6,8 @@ export const ALWAYS_PENALTIES = [
 
 export const RANDOM_PENALTIES = [
     { text: 'Не пить пенное', probability: 5 },
-    { text: 'Пьёшь два глотка пенного', probability: 5 },
-    { text: 'Пьёшь три глотка пенного', probability: 3 },
+    { text: 'Пьёшь два глотка пенного', probability: 50 },
+    { text: 'Пьёшь три глотка пенного', probability: 25 },
     { text: 'Пьёшь пол стопки крепкого один раз за гонку', probability: 2 },
     { text: 'Все игроки пьют по одному глотку пенного', probability: 10 },
     { text: 'Разрешён перезапуск гонки для тебя', probability: 5 },
@@ -16,4 +16,5 @@ export const RANDOM_PENALTIES = [
     { text: 'Другой игрок пьёт за тебя', probability: 5 },
     { text: 'Все игроки пьют по два глотка пенного', probability: 5 },
     { text: 'Можешь выбрать любой транспорт', probability: 10 },
+    { text: 'ВЫБОР! По два глотка пенного в этой гонке и + иммунитет | Играть по правилам', probability: 10 },
 ];
